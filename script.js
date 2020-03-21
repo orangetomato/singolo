04c14aae90a22999b248cc289a8981230314702c
orangetomato <itaintme@mail.ru>
2020-03-21 18:07:20 +0300

    feat: implement new functions

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
             for (let link of linkList) {
                 if (link.classList.contains('navigation-menu__link--current')) {
                     link.classList.remove('navigation-menu__link--current');
+                    break;
                 }
             }
             evt.target.classList.add('navigation-menu__link--current');
@@ -16,6 +17,26 @@
     }
 
     navigation.addEventListener('click', changeColor);
+
+
+    document.addEventListener('scroll', changeActiveLink);
+    window.onload = changeActiveLink();
+
+    function changeActiveLink() {
+        const currentPositionY = window.scrollY;
+        const tagsWithId = document.querySelectorAll('[id]');
+
+        tagsWithId.forEach(tag => {
+            if (tag.offsetTop - 95 <= currentPositionY && tag.offsetTop + tag.offsetHeight - 95 > currentPositionY) {
+                linkList.forEach(link => {
+                    link.classList.remove('navigation-menu__link--current');
+                    if (tag.getAttribute('id') === link.getAttribute('href').substring(1)) {
+                        link.classList.add('navigation-menu__link--current');
+                    }
+                });
+            }
+        });
+    }
 })();
 
 (function() {
@@ -81,4 +102,84 @@
     for (let phone of phoneList) {
         phone.addEventListener('click', switchScreen);
     }
+})();
+
+(function() {
+    const buttons = document.querySelectorAll('.portfolio__button');
+    const portfolio = document.querySelector('.portfolio__picture-list');
+
+    function changePictures(evt) {
+        if (!evt.target.classList.contains('portfolio__button--active')) {
+            let portfolioPictures = [...portfolio.querySelectorAll('.portfolio__item')];
+            portfolioPictures.unshift(portfolioPictures.pop());
+            portfolioPictures.forEach(picture => portfolio.append(picture));
+        }
+        buttons.forEach(button => button.classList.remove('portfolio__button--active'));
+        evt.target.classList.add('portfolio__button--active');
+    }
+
+    buttons.forEach(button => button.addEventListener('click', () => changePictures(event)));
+})();
+
+(function() {
+    let isSwitched = true;
+    const portfolio = document.querySelector('.portfolio__picture-list');
+    const portfolioPictures = portfolio.querySelectorAll('.portfolio__picture');
+
+    function showBorder(evt) {
+        if (evt.target.classList.contains('portfolio__picture--bordered')) {
+            isSwitched = false;
+        }
+
+        portfolioPictures.forEach(picture => picture.classList.remove('portfolio__picture--bordered'));
+        
+        if (isSwitched) {
+            evt.target.classList.add('portfolio__picture--bordered');
+        }
+
+        isSwitched = true;
+    }
+
+    portfolioPictures.forEach(image => image.addEventListener('click', () => showBorder(event)));
+})();
+
+(function() {
+    const modal = document.querySelector('.modal');
+    const sendFormButton = document.querySelector('.contact-form__button');
+    const closeModalButton = document.querySelector('.modal__close-button');
+    const subject = document.querySelector('.modal__subject');
+    const description = document.querySelector('.modal__description');
+
+    function addNodeValue(node, defaultValue) {
+        let value = document.querySelector(node).value;
+        value = value === '' ? defaultValue : value;
+        return value;
+    }
+
+    function showModal() {
+        modal.classList.remove('hidden');
+        addContent(event);
+    }
+
+    function hideModal() {
+        modal.classList.add('hidden');
+        document.forms[0].reset();
+    }
+
+    function addContent() {
+        subject.innerText = `Subject: ${addNodeValue('.contact-form__item--subject', 'no subject')}`;
+        description.innerText = `Description: ${addNodeValue('.contact-form__item--textarea', 'no description')}`;
+    }
+
+    function validate(evt) {
+        let requiredFields = [...document.querySelectorAll('[required]')];
+        let isValid = field => field.checkValidity();
+        if (requiredFields.every(isValid)) {
+            evt.preventDefault();
+            showModal();
+        }
+    }
+
+    sendFormButton.addEventListener('click', () => validate(event));
+    closeModalButton.addEventListener('click', hideModal);
 })();
